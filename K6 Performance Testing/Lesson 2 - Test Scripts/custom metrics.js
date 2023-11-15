5ef1368635392dacd84acddb66432452de30b4e2
DavidTzemach <dzcomp@gmail.com>
2023-11-15 15:01:51 +0200

    last

--- K6 Performance Testing/Lesson 2 - Test Scripts/custom metrics.js
+++ K6 Performance Testing/Lesson 2 - Test Scripts/custom metrics.js
@@ -1,24 +1,22 @@
 import http from 'k6/http';
-import { check } from 'k6';
 import { sleep } from 'k6';
-import { Metric } from 'k6/metrics';
+import { Counter } from 'k6/metrics';//Import the k6/metrics module.
 
 export const options = {
-    vus: 10,
-    duration: '10s',
+    vus: 5,
+    duration: '5s',
     thresholds: {
-        //http_req_duration: ['p(95)<200'],
-        //http_req_failed: ['rate<0.01'],
-        http_reqs: ['count>20'],
-        http_reqs: ['rate<6']   
+        http_req_duration: ['p(95)<250'],
+        my_counter: ['count>10']
     }
 }
 
+//the following creates a custom trend. The object in the script is called myTrend, 
+//and its metric appears in the results output as waiting_time.
+let myCounter = new Counter('my_counter');//Step 2
+
 export default function () {
     const res = http.get('https://test.k6.io/');
-    check(res, {
-        'status is 200': (r) => r.status === 200,
-        'page is startpage': (r) => r.body.includes('Collection of simple web-pages')
-    });
+    myCounter.add(1);//In the VU iteration code, use the add method to take a measurement.
     sleep(2);
 }
