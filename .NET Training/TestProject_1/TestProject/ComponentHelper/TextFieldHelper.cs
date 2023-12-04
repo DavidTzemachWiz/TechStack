9b7d131f2cdc3193262c1dc269867553b424a182
DavidTzemach <dzcomp@gmail.com>
2023-12-04 14:54:40 +0200

    Test
    asd

---
+++ .NET Training/TestProject_1/TestProject/ComponentHelper/TextFieldHelper.cs
@@ -0,0 +1,28 @@
+﻿using MStest_TMP.Settings;
+using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MStest_TMP.ComponentHelper
+{
+    public class TextFieldHelper
+    {
+        private static IWebElement element;
+        public static void  TypeInTextBox (By locator, string Text)
+        {
+           element = SiteElementValidations.GetElement(locator);
+           element.SendKeys(Text);
+
+        }
+
+        public static void ClearText(By locator)
+        {
+            element = SiteElementValidations.GetElement(locator);
+            element.Clear();
+
+        }
+    }
+}
--- .NET Training/TestProject_1/TestProject/Test Scripts/TextBox.cs
+++ .NET Training/TestProject_1/TestProject/Test Scripts/TextBox.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using MStest_TMP.ComponentHelper;
+using MStest_TMP.Settings;
+using OpenQA.Selenium;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +9,28 @@ using System.Threading.Tasks;
 
 namespace MStest_TMP.Test_Scripts
 {
-    internal class TextBox
+    [TestClass]
+    
+    public class TextBox
     {
+        [TestMethod]
+        public void TestTextBox()
+        {
+            NevigationHelper.NevigateToSite(ObjectRepository.Config.GetUrl());
+
+            //Full Link Text 
+            IWebElement FillABug_1 = ObjectRepository.Driver.FindElement(By.LinkText("File a Bug"));
+            FillABug_1.Click();
+
+            //Add credentials from config file
+            //ObjectRepository.Driver.FindElement(By.Id("Bugzilla")).SendKeys(ObjectRepository.Config.GetUserName());
+            //ObjectRepository.Driver.FindElement(By.Id("Bugzilla_password")).SendKeys(ObjectRepository.Config.GetUserPass());
+
+            TextFieldHelper.TypeInTextBox(By.Id("Bugzilla_login"), ObjectRepository.Config.GetUserName());
+            TextFieldHelper.TypeInTextBox(By.Id("Bugzilla_password"), ObjectRepository.Config.GetUserPass());
+
+
+            Thread.Sleep(3000);
+        }
     }
 }
